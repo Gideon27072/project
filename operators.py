aa0c14781b5e1692243da50aa707555b73f2d437
Gideon27072 <ukogideon13@gmail.com>
2022-07-31 04:30:12 -0700

    operators

---
+++ operators.py
@@ -0,0 +1,17 @@
+#Arithmetric operators
+a = 6
+b = 5
+
+print(a+b)
+print(9+b)
+print(a%b)
+print(a//b)
+
+#Assignment operator
+c = 4
+c+=a
+print(c)
+
+#comparison operators
+print(a == b)
+print(a != b)
