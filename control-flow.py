f202b51840a4669f90462784a734b4cb762b28d1
Gideon27072 <ukogideon13@gmail.com>
2022-08-01 04:37:19 -0700

    control-flow.py

---
+++ control-flow.py
@@ -0,0 +1,26 @@
+#if statements
+#we are testing an expression to return true
+
+a = 3
+# if a >= 1:
+#     print(a, "is a positive number")
+#     print("it is also an integer")
+# print("thank you!")
+
+#if else statement
+
+# if a < 1:
+#     print(a, "is less 1")
+# else:
+#     print(a, "is greater than 1")
+
+#if elif else statement
+#elif checks multiple expression for true
+
+if a < 1:
+    print(a, "is less than 1")
+elif a == 0:
+    print(a, "is a null value")
+else:
+    print(a, "is greater than 1")
+         
