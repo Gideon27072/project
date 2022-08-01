652c90e027401712f1f90aa606677c6bee2779c5
Gideon27072 <ukogideon13@gmail.com>
2022-08-01 12:08:46 -0700

    added function

--- hello_world.py
+++ hello_world.py
@@ -1,12 +1,12 @@
 #This is a python hello world program
-#print('hello world')
+print('hello world')
 
 #create a multi line string
 
 #declare variables
-#greeting = 'good morning'
-#print(greeting)
+greeting = 'good morning'
+print(greeting)
 
 #data types
-x = None
+x = ("how are you")
 print(type(x))
