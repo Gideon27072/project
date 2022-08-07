42513166ab213aecc523829c66034bc42cd70ccf
Gideon27072 <ukogideon13@gmail.com>
2022-08-07 09:45:42 -0700

    class

---
+++ classes.py
@@ -0,0 +1,11 @@
+#inbuilt class
+
+num = 8.0
+print(type(num))
+
+#user defined classes
+class HousingEstate:
+    #classes hold attributes and methods
+    pass
+house_1 = HousingEstate()
+print(type(house_1))
